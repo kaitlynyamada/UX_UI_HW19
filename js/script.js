039b0bb451d48ffc606aeb4b38951fe68e8f20b7
kaitlynyamada <114835342+kaitlynyamada@users.noreply.github.com>
2023-04-16 17:25:20 -0700

    added ui interactions to card and buttons
    still not able to get mobile nav and primary nav working properly

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
-
+/*mobile menu*/
 $('.mobile-menu').on('click', function() {
     $('.nav-mobile').slideToggle();
 });
+
+/*
+===================================================================
+card interactions
+====================================================================*/
+/*water U doing*/
+$('.works-content__water').hover(function() {
+    $(this).addClass('shadow-drop-bottom');
+}, function() {
+    $(this).removeClass('shadow-drop-bottom');
+}
+);
+
+$('.works-content__sara').hover(function() {
+    $(this).addClass('shadow-drop-bottom');
+}, function() {
+    $(this).removeClass('shadow-drop-bottom');
+}
+);
+
+/*
+===================================================================
+button interactions
+====================================================================*/
+$('.button-primary').hover(function() {
+    $(this).addClass('button-hover');
+}, function() {
+    $(this).removeClass('button-hover');
+}
+);
